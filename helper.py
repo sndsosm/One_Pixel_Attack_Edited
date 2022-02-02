1359f1534b0fd6ac57f2f6ae8390aeb9e89c8bd1
sndsosm <82066824+sndsosm@users.noreply.github.com>
2022-02-02 09:02:31 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -250,9 +250,9 @@ def heatmap(df):
                'perturbation','perturbed']
     results_table = pd.DataFrame(df, columns=columns)
     Y_pred = results_table.predicted
-    y_pred = np.array(Y_pred, axis=1)
+    y_pred = np.array(Y_pred)
     Y_test =  results_table.true
-    y_test=np.array(Y_test, axis=1)
+    y_test=np.array(Y_test)
     for ix in range(len(y_pred.iloc[0])):
         print(ix, confusion_matrix(np.argmax(y_test,axis=1),y_pred)[ix].sum())
     cm = confusion_matrix(np.argmax(y_test,axis=1),y_pred)
