c57aeff60e9de553fe2acc1f25725fca18a34e38
sndsosm <82066824+sndsosm@users.noreply.github.com>
2022-02-02 09:13:57 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -253,13 +253,13 @@ def heatmap(df):
     y_pred = np.array(Y_pred)
     Y_test =  results_table.true
     y_test=np.array(Y_test)
-    for ix in range(len(y_pred.iloc[0])):
+    for ix in range(len(y_pred)):
         print(ix, confusion_matrix(np.argmax(y_test,axis=1),y_pred)[ix].sum())
     cm = confusion_matrix(np.argmax(y_test,axis=1),y_pred)
     print(cm)
 
-    df_cm = pd.DataFrame(cm, range(len(y_pred.iloc[0])),
-                       range(len(y_pred.iloc[0])))
+    df_cm = pd.DataFrame(cm, range(len(y_pred)),
+                       range(len(y_pred)))
     plt.figure(figsize = (10,7))
     sn.set(font_scale=1.4)#for label size
     sn.heatmap(df_cm, annot=True,annot_kws={"size": 12})# font size
