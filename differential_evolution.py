77bbe5f173ec119652b44c196a8b4211322a69a1
sndsosm <82066824+sndsosm@users.noreply.github.com>
2022-02-14 00:23:20 +0200

    Update differential_evolution.py

--- differential_evolution.py
+++ differential_evolution.py
@@ -15,7 +15,7 @@ import numpy as np
 from scipy.optimize import OptimizeResult, minimize
 from scipy.optimize.optimize import _status_message
 from scipy._lib._util import check_random_state
-from scipy._lib.six import string_types
+from dipy.utils.six import xrange,string_types
 import warnings
 
 
